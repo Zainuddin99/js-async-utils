c9d2b2287fd8fac07f97059e28c2ca589698bbf9
Zainuddin99 <zainuzayn11@gmail.com>
2022-07-29 16:23:53 +0530

    Added promiseWrapper code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+//All utilities
+const promiseWrapper = (fn: () => any) => {
+  return (...args: any[]) =>
+    new Promise(async (resolve, reject) => {
+      try {
+        const response = await fn(...(args as []));
+        resolve(response);
+      } catch (error) {
+        reject(error);
+      }
+    });
+};
 
+module.exports = { promiseWrapper };
